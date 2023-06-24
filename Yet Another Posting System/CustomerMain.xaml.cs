b1c4989d69fa16372cbea3500af4271faf2be6bb
Milad Zarei <miliaxe0@gmail.com>
2023-06-24 16:53:03 +0330

    Initial commit

---
+++ Yet Another Posting System/CustomerMain.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Yet_Another_Posting_System
+{
+    /// <summary>
+    /// Interaction logic for CustomerMain.xaml
+    /// </summary>
+    public partial class CustomerMain : Window
+    {
+        public CustomerMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
