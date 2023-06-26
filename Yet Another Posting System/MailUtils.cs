3f7021e8e7b859f02c47551c123ceaf203ff68f3
Milad Zarei <miliaxe0@gmail.com>
2023-06-26 18:02:22 +0330

    Add Mailing capability + CustomerSignup UI

---
+++ Yet Another Posting System/MailUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yet_Another_Posting_System
+{
+    internal class MailUtils
+    {
+    }
+}
