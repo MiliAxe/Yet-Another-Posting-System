f167a6949d152e0969f6c3d3e23af3f0ef94ff56
Milad Zarei <miliaxe0@gmail.com>
2023-07-04 21:43:30 -0700

    Added customer searchorder button

--- Yet Another Posting System/Utils/SQLUtils.cs
+++ Yet Another Posting System/Utils/SQLUtils.cs
@@ -204,6 +204,25 @@ namespace Yet_Another_Posting_System.Utils
             this.dtConnection.Close();
             return result;
         }
+        
+        public string GetCustomerIdFromUsername(string username)
+        {
+            string result = "";
+
+            string selectQuery = $"SELECT ID FROM Users WHERE Username = '{username}';";
+            this.dtConnection.Open();
+            using (SQLiteCommand selectCommand = new SQLiteCommand(selectQuery, this.dtConnection))
+            {
+                SQLiteDataReader reader = selectCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    result = reader.GetValue(0).ToString(); 
+                }
+            }
+
+            this.dtConnection.Close();
+            return result;
+        }
 
         public string GetCustomerIdFromUsername(string username)
         {
